70fea403d1933fac946e983cb8087c37cd59699a
Christian Walpen <c.walpen@gmx.net>
2025-07-10 09:18:26 +0200

    Domain - Rename command parameter ConfigFile to Scenario

--- src/Avalanche.Tool/LoadTestOption.cs
+++ src/Avalanche.Tool/LoadTestOption.cs
@@ -16,8 +16,8 @@ namespace Avalanche
     [Verb("loadtest", true, HelpText = "Start a load test")]
     public class LoadTestOption
     {
-        [Option('f', "configfile", HelpText = "UNC Path to the Configfile", Required = false)]
-        public string ConfigFile { get; set; }
+        [Option('s', "scenario", HelpText = "UNC Path to the Scenario YML file. The filename has to be the same as the scenario", Required = false)]
+        public string Scenario { get; set; }
 
         [Option('u', "url", HelpText = "Url to the Avalanche server", Required = false)]
         public string Url { get; set; }
@@ -26,14 +26,14 @@ namespace Avalanche
 
         public void Execute()
         {
-            if (string.IsNullOrEmpty(ConfigFile))
+            if (string.IsNullOrEmpty(Scenario))
             {
-                Console.WriteLine("The Parameter --configfile or -f has to be provided with the path to the config file");
+                Console.WriteLine("The Parameter --scenario or -s has to be provided with the path to the config file");
 
-                ConfigFile = "LoadTest";
+                Scenario = "LoadTest";
             }
 
-            Console.WriteLine($"Scenario: {ConfigFile}");
+            Console.WriteLine($"SCENARIO: {Scenario}");
 
 #if DEBUG
             // in debug wait until the website is started
@@ -84,14 +84,14 @@ namespace Avalanche
             using (var dispatcher = new CommandDispatcher(eventBus))
             {
                 // LoadTest
-                var path = GetFilePath(ConfigFile);
+                var path = GetFilePath(Scenario);
 
                 var facade = new TestFacade(dispatcher, LoggerFactory);
                 var scenario = facade.GetScenario(path);
 
                 eventBus.Send(new InitScenarioEvent
                 {
-                    Name = ConfigFile,
+                    Name = Scenario,
                     Tests = scenario.Tests.Select(t => new WriteModel.Events.TestConfig
                     {
                         Name = t.Name,
@@ -111,7 +111,7 @@ namespace Avalanche
                     })
                 });
 
-                facade.Start(ConfigFile, scenario);
+                facade.Start(Scenario, scenario);
 
                 //
                 // Give the collector some time to finish the work
