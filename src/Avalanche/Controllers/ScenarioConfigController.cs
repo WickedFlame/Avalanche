90aac53cf27811da7248db8374f2e269cb58a8d7
Christian Walpen <c.walpen@gmx.net>
2025-07-11 11:59:19 +0200

    #39 Create new Scenarios from the Web UI

---
+++ src/Avalanche/Controllers/ScenarioConfigController.cs
@@ -0,0 +1,51 @@
+﻿using Avalanche.Domain;
+using Avalanche.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Avalanche.Controllers
+{
+    public class ScenarioConfigController : Controller
+    {
+        public IActionResult Index(string scenario)
+        {
+            var path = PathMapper.GetScenarioPath();
+
+            var scenarios = Directory.GetFiles(path, "*.yml");
+            
+            var model = new ScenarioConfigModel
+            {
+                Scenarios = scenarios.Select(s => Path.GetFileName(s).Replace(".yml", "")),
+                Scenario = LoadFile(scenario)
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult SaveScenario([FromForm]RawScenarioModel model)
+        {
+            var file = $"{PathMapper.GetScenarioPath()}/{model.Name}.yml";
+            System.IO.File.WriteAllText(file, model.RawContent);
+            return RedirectToAction("Index", new { Scenario = model.Name });
+        }
+
+        private static RawScenarioModel LoadFile(string scenario)
+        {
+            if(string.IsNullOrEmpty(scenario))
+            {
+                return new RawScenarioModel
+                {
+                    Name = string.Empty,
+                    RawContent = string.Empty
+                };
+            }
+
+            var file = PathMapper.GetScenarioFile(scenario);
+            return new RawScenarioModel
+            {
+                Name = scenario,
+                RawContent = System.IO.File.ReadAllText(file),
+            };
+        }
+    }
+}
---
+++ src/Avalanche/Models/ScenarioConfigModel.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Avalanche.Models
+{
+    public class ScenarioConfigModel
+    {
+        public IEnumerable<string> Scenarios { get; set; }
+
+        public RawScenarioModel Scenario { get; set; }
+    }
+
+    public class RawScenarioModel
+    {
+        public string Name { get; set; }
+
+        public string RawContent { get; set; }
+    }
+}
