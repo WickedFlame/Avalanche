0aab097ffc1b6d84ef6b0998477b926278fa34ce
Christian Walpen <c.walpen@gmx.net>
2025-10-16 09:06:56 +0200

    DefaultAuthenticationHandler for none

---
+++ src/Avalanche.Runner/Handlers/DefaultAuthenticationHandler.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Avalanche.Runner.Handlers
+{
+    public class DefaultAuthenticationHandler : IExecutionHandler
+    {
+        public void Execute(MeasureMap.ExecutionContext context, Scenario settings)
+        {
+            //
+            // DO nothin here 
+        }
+    }
+}
